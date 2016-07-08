111b5227ca2cfef6040c37c1c0eb70f5fa966eec
Emanuel Martínez <emartinezpinzon@gmail.com>
2016-07-08 00:30:54 -0500

    __str__ en los modelos y mejoras en las relaciones

--- apps/adopcion/models.py
+++ apps/adopcion/models.py
@@ -7,3 +7,6 @@ class Persona(models.Model):
     telefono = models.CharField(max_length=12)
     email = models.EmailField()
     domicilio = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
