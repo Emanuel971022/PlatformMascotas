dbd8974f9a56dde65f4ae85beabef3141f27ae6c
Emanuel Martínez <emartinezpinzon@gmail.com>
2016-07-11 02:29:37 -0500

    Se listan las mascotas
    Agregada la vista (Y demás) que permite ver las mascotas registradas en la base de datos.

--- apps/mascota/views.py
+++ apps/mascota/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from .forms import MascotaForm
+from .models import Mascota
 
 def index(request):
     return render(request, 'mascota/index.html')
@@ -15,3 +15,8 @@ def mascota_view(request):
         form = MascotaForm()
 
     return render(request, 'mascota/mascota_form.html', {'form': form})
+
+def mascota_list(request):
+    mascota = Mascota.objects.all()
+    context = {'mascotas':mascota}
+    return render(request, 'mascota/mascota_list.html', context)
